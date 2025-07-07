1ec4910f62e31d64ced35fa835e72dfa61762263
Tyler Kozlowski <Tyler.J.Kozlowski@gmail.com>
2025-07-07 15:02:01 -0500

    added linter scripts for rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri_app_lib;
-
 fn main() {
     tauri_app_lib::run()
 }
